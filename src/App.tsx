c6494725fd31191f1d8d711aff8d13de461a8933
ClintZy101 <clielizek.joy@gmail.com>
2023-11-09 12:52:10 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import BasicExample from './components/Accordion';
+
+// Store
+interface CounterState {
+  value: number;
+}
+interface UserState {
+  isSignedIn: boolean;
+}
+
+// Actions
+const incrementByAmount = {type: 'INCREMENT', payload: 10}
+const decrement = {type: 'DECREMENT'}
+
+// Reducers
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      esdfsd
+      <BasicExample />
     </div>
   );
 }
