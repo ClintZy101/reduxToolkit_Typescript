d76cb8b4f9bc1d81c6b27e51c21cc2323c24dab4
ClintZy101 <clielizek.joy@gmail.com>
2023-11-10 12:56:18 +0900

    working with createAsyncThunk

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment, decrement, incrementByAmount } from '../state/features/counter/counterSlice'
-import { RootState } from '../state/store'
+import { increment, decrement, incrementByAmount, incrementAsync } from '../state/features/counter/counterSlice'
+import { AppDispatch, RootState } from '../state/store'
 
 export default function Counter() {
     const count = useSelector((state: RootState) => state.counter.value)
-    const dispatch = useDispatch()
+    const dispatch = useDispatch<AppDispatch>()
     
     
   return (
@@ -14,6 +14,7 @@ export default function Counter() {
         <h2>{count}</h2>
         <div className=''>
             <button onClick={() => dispatch(increment())}> Increment</button>
+            <button onClick={() => dispatch(incrementAsync(20))}> Increment Async</button>
             <button onClick={() => dispatch(incrementByAmount(10))}> IncrementBY10</button>
             <button onClick={() => dispatch(decrement())} > Decrement1</button>
         </div>
